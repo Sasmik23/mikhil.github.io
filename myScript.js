dae955491820551efaa655c17a7baf12b0b4972b
Sasmik23 <36692672+Sasmik23@users.noreply.github.com>
2018-04-18 16:32:35 +0800

    java update

---
+++ myScript.js
@@ -0,0 +1,4 @@
+function myFunction(){
+  document.getElementById("demo").src="https://i.pinimg.com/736x/fa/b4/67/fab467a399c1aabce6600fe2d736e126--hand-guns-natasha-romanoff.jpg";
+  document.getElementById("demo").style.height = "350px";
+}
